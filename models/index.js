7cc6a69eb9030f2a2fb15af766496e5c1fdd382d
MikeAdu <mike_adusah@yahoo.com>
2021-08-20 16:18:27 -0700

    added sequlize to index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: "categoryId"
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+foreignKey: "productId"
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+through:ProductTag, foreignKey: "productId"
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+through:ProductTag, foreignKey: "tagId"
+});
 module.exports = {
   Product,
   Category,
